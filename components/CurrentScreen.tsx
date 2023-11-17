47543714d0172798b2d5b70c6fb11b2392aeacce
klimjs <klimsemenov86@gmail.com>
2023-11-17 21:43:19 +0100

    remove screen button

--- components/CurrentScreen.tsx
+++ components/CurrentScreen.tsx
@@ -1,13 +1,26 @@
-import { Text } from '@mantine/core';
+import { ActionIcon, Group, Text } from '@mantine/core';
+import { IconTrash } from '@tabler/icons-react';
 
 export const CurrentScreen = () => {
   console.log('current');
 
   return (
     <>
-      <Text fw={700} size="sm" tt="uppercase">
-        Page 26
-      </Text>
+      <Group justify="space-between">
+        <Text fw={700} size="sm" tt="uppercase">
+          Page 26
+        </Text>
+
+        <ActionIcon
+          color="red"
+          variant="outline"
+          radius="xl"
+          aria-label="Remove screen"
+          loading={false}
+        >
+          <IconTrash style={{ width: '70%', height: '70%' }} stroke={1.5} />
+        </ActionIcon>
+      </Group>
     </>
   );
 };

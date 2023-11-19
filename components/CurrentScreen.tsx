3bfad2604464c77e48eaf9bcf5a19a6adad8d22a
klimjs <klimsemenov86@gmail.com>
2023-11-19 15:05:01 +0100

    remove screen

--- components/CurrentScreen.tsx
+++ components/CurrentScreen.tsx
@@ -1,10 +1,23 @@
+import { setCurrentScreenId } from '@/redux/screen';
 import { RootState } from '@/redux/store';
 import { ActionIcon, Group, Text } from '@mantine/core';
 import { IconTrash } from '@tabler/icons-react';
-import { useSelector } from 'react-redux';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const CurrentScreen = () => {
+  const queryClient = useQueryClient();
   const { currentId } = useSelector((state: RootState) => state.screen);
+  const dispatch = useDispatch();
+
+  const { mutate: remove, isPending: isRemoving } = useMutation({
+    mutationFn: () => axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/screens/${currentId}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['screens'] });
+      dispatch(setCurrentScreenId(null));
+    },
+  });
 
   return (
     <>
@@ -14,11 +27,12 @@ export const CurrentScreen = () => {
         </Text>
 
         <ActionIcon
+          onClick={() => remove()}
           color="red"
           variant="outline"
           radius="xl"
           aria-label="Remove screen"
-          loading={false}
+          loading={isRemoving}
         >
           <IconTrash style={{ width: '70%', height: '70%' }} stroke={1.5} />
         </ActionIcon>
